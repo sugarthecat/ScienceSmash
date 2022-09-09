bd5bc7ed0c06c2b1082b7085255bf6ec4a9983ad
TJ Nickerson <TJPNick@gmail.com>
2022-09-09 09:14:55 -0400

    Added backgrund fade to screen, placeholder

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let fade = 0
+let fadeDir = -2
 function setup(){
     createCanvas(windowWidth,windowHeight)
 }
 function draw(){
-    background(0)
+    background(fade)
+    if(fade >= 255 || fade <= 0){
+        fadeDir *= -1
+    }
+    fade += fadeDir
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
