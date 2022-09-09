aa52564492664e34913639d29bd78eefa9e317fa
TJ Nickerson <TJPNick@gmail.com>
2022-09-09 10:47:45 -0400

    Added player & player movement

--- player.js
+++ player.js
@@ -1,8 +1,53 @@
 class Player{
-    constructor(){
-
+    constructor(x,y){
+        this.x = x
+        this.y = y
+        this.w = 30 // width, constant
+        this.h = 50 // height, constant
+        this.dirx = 0 //1, 0, or -1, representing direction x
+        this.diry = 0//1,0, or -1, representing direction y
+    }
+    //Ensures that dirx and diry are 
+    fixDirections(){
+        //Checks if either activating key is down for directional movement
+        let right = keyIsDown(RIGHT_ARROW) || keyIsDown(68) // D key
+        let left = keyIsDown(LEFT_ARROW) || keyIsDown(65) // A key
+        let up = keyIsDown(UP_ARROW) || keyIsDown(87) // W key
+        let down = keyIsDown(DOWN_ARROW) || keyIsDown(83) // S key
+        //If only one of the x directions, or only one of the y directions is down, direction is set as that.
+        this.dirx = 0
+        this.diry = 0
+        if(right && !left){
+            this.dirx = 1
+            //if a right key is pressed, and a left key is not pressed, x direction is positive
+        }else if(left && !right){
+            this.dirx = -1
+            //if a left key is pressed, and a right key is not pressed, x direction is negative
+        }
+        if(down && !up){
+            this.diry = 1
+            //if a down key is pressed, and a up key is not pressed, y direction is positive
+        }else if(up && !down){
+            this.diry = -1
+            //if a up key is pressed, and a down key is not pressed, y direction is negative
+        }
+        
+    }
+    runMoveTick(){
+        this.fixDirections()
+        const moveSpeed = 5
+        if(this.dirx != 0 && this.diry != 0){
+            this.x += this.dirx * moveSpeed  / sqrt(2)
+            this.y += this.diry * moveSpeed  / sqrt(2)
+            // accounts for pythagorean theorem if multi-axis movement
+        }else{
+            this.x += this.dirx * moveSpeed
+            this.y += this.diry * moveSpeed
+        }
     }
     draw(){
-        
+        noStroke()
+        fill(255,0,0)
+        rect(this.x,this.y,this.w,this.h)
     }
 }
--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-let fade = 0
-let fadeDir = -2
+let player = new Player(0,0)
 function setup(){
     createCanvas(windowWidth,windowHeight)
+    frameRate(60)
 }
 function draw(){
-    background(fade)
-    if(fade >= 255 || fade <= 0){
-        fadeDir *= -1
-    }
-    fade += fadeDir
+    background(0)
+    player.runMoveTick()
+    player.draw()
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
