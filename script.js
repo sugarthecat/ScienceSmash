6054a96d0e0d13f375e6821ee47dd9cda1e5401c
TJ Nickerson <TJPNick@gmail.com>
2022-09-19 11:00:56 -0400

    readded camera because it went bye bye

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let player = new Player(0,0)
-
+let player 
+let camera
 function setup(){
+    player = new Player(windowWidth/2,windowHeight/2)
+    camera = new Camera(windowWidth/2,windowHeight/2)
     createCanvas(windowWidth,windowHeight)
     frameRate(60)
 }
-
 function draw(){
     background(0)
     player.runMoveTick()
+    camera.target(player)
     player.draw()
 }
-
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
