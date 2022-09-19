061769a87503ccfc34bfde201fc9014c9672b722
Toby <thansen2024@jpkeefehs.org>
2022-09-19 08:53:48 -0400

    better comments

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let player 
-let camera
+let player = new Player(0,0)
+
 function setup(){
-    player = new Player(windowWidth/2,windowHeight/2)
-    camera = new Camera(windowWidth/2,windowHeight/2)
     createCanvas(windowWidth,windowHeight)
     frameRate(60)
 }
+
 function draw(){
     background(0)
     player.runMoveTick()
-    camera.target(player)
     player.draw()
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
