0f9579f38a2f8d48898fdfa5c52a058696447964
TJ Nickerson <TJPNick@gmail.com>
2022-09-22 09:07:06 -0400

    added icon

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function setup(){
   player = new Player(500,500)
   camera = new Camera(player.x-windowWidth/2,player.y-windowHeight/2)
   song = loadSound('music/sneaky_snitch.mp3',fileLoaded)
-  song.play();
+  //song.play();
   createCanvas(windowWidth,windowHeight)
   frameRate(60)
   angleMode(DEGREES)
