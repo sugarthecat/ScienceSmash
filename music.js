96b9c32c9a1a8f780f417780ec0007b8a81a7511
Toby <thansen2024@jpkeefehs.org>
2022-09-22 08:46:39 -0400

    Fixed 'Sneaky Snitch" audio (just a placeholder)

--- music.js
+++ music.js
@@ -3,10 +3,3 @@
 // Music promoted by https://www.chosic.com/free-music/all/
 // Creative Commons CC BY 3.0
 // https://creativecommons.org/licenses/by/3.0/
-<<<<<<< HEAD
-=======
-
-function playAudio() {
-    //sneakySnitch.play();
-  }
->>>>>>> 76d1195a0585f501dca2e7fea28712c189fc1d32
