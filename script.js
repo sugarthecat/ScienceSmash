ee8c9e6efe774eb470ca44fe8df2954ffbb0ed87
TJ Nickerson <TJPNick@gmail.com>
2022-09-23 08:44:12 -0400

    changed testing world to show inverse corners

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function setup(){
   //nanner garage
   for(let x = 0; x<22; x++){
     for(let y = 0; y<22; y++){
-      if(x == 0 || y == 0 || y == 10 || y == 9 || y == 11 || y == 21 || x == 21){
+      if(x == 0 || y == 0 || ((y == 10 || y == 9 || y == 11) && !(x == 9 || x == 10 || x == 11)) || y == 21 || x == 21){
         level.addTile(new CollisionTile(images.bananas[0]),x,y)
       }else{
         level.addTile(new Tile(images.bananas[1]),x,y)
