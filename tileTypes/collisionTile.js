10a26f303810f7e27b1c9fa3ba68b241e9d2fb3c
TJ Nickerson <TJPNick@gmail.com>
2022-10-06 14:00:32 -0400

    test2

--- tileTypes/collisionTile.js
+++ tileTypes/collisionTile.js
@@ -10,6 +10,7 @@ class CollisionTile extends Tile{
         this.hasGround = false
         
     }
+    //different collision function for collisiontile subclass than default tile superclass
     collides(other){
         return (this.x +this.w > other.x && other.x + other.w > this.x && this.y +this.h > other.y && other.y + other.h > this.y);
     }
