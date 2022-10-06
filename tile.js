4ddbeebff21829c8b48cd29d16ef8ed6f5567fb2
TJ Nickerson <TJPNick@gmail.com>
2022-10-06 13:50:55 -0400

    moved wall draw functions to tile

--- tile.js
+++ tile.js
@@ -7,11 +7,30 @@ class Tile{
         this.img = img
         this.hasUpper = false
         this.hasGround = true
+        this.hasRight = false;
+        this.hasLeft = false;
     }
     displayGround(){
         image(this.img,this.x,this.y,this.w,this.h)
     }
     collides(a){
         return false
+    }
+    
+    displayRight(){
+        push()
+        rotate(60)
+        scale(1,TILE_SCALE)
+        rotate(-45)
+        image(this.img,this.x-this.y-this.w,this.y,this.w,this.h)
+        pop()
+    }
+    displayLeft(){
+        push()
+        rotate(-60)
+        scale(1,TILE_SCALE)
+        rotate(45)
+        image(this.img,this.x-this.y,this.x,this.w,this.h)
+        pop()
     }
 }
--- tileTypes/collisionTile.js
+++ tileTypes/collisionTile.js
@@ -13,20 +13,4 @@ class CollisionTile extends Tile{
     collides(other){
         return (this.x +this.w > other.x && other.x + other.w > this.x && this.y +this.h > other.y && other.y + other.h > this.y);
     }
-    displayRight(){
-        push()
-        rotate(60)
-        scale(1,TILE_SCALE)
-        rotate(-45)
-        image(this.img,this.x-this.y-this.w,this.y,this.w,this.h)
-        pop()
-    }
-    displayLeft(){
-        push()
-        rotate(-60)
-        scale(1,TILE_SCALE)
-        rotate(45)
-        image(this.img,this.x-this.y,this.x,this.w,this.h)
-        pop()
-    }
 }  
