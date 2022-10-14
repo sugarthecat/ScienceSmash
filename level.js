caceda11b879821ecdaeb72d2beacf5709347e05
TJ Nickerson <TJPNick@Gmail.com>
2022-10-14 16:00:26 -0400

    allowed target display on entities

--- level.js
+++ level.js
@@ -110,6 +110,19 @@ class Level{
         }
         return false;
     }
+    collidesGround(other){
+        if(other.x < 0 || other.y < 0 || other.x > this.tiles.length*100 || other.y > this.tiles[0].length*100){
+            return true
+        }
+        for(let x = 0; x<this.tiles.length; x++){
+            for(let y = 0; y<this.tiles.length; y++){
+                if(this.tiles[x][y] && this.tiles[x][y].collides(other)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
     displayTarget(){
         this.targetRotation+=1.5
@@ -120,7 +133,7 @@ class Level{
         let targetAngle = atan2(disx,disy)+45
         disx = sin(targetAngle)*xydist
         disy = cos(targetAngle)*xydist
-        if(!this.collides({x:disx, y:disy, w:0, h:0})){
+        if(!this.collidesGround({x:disx, y:disy, w:0, h:0})){
             push()
         
             translate (disx,disy)
