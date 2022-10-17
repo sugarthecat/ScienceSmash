c85468e5481e6f23d85d79a10a3fd5f4d3423340
TJ Nickerson <TJPNick@gmail.com>
2022-10-17 13:59:57 -0400

    Tweaks with toby's code

--- proceduralLevelGeneration.js
+++ proceduralLevelGeneration.js
@@ -1,14 +1,26 @@
-let levelGrid = [99,99];
-let rnd = [standard, standard, standard, standard, standard, standard, standard, standard, loot, shop];
+
 
 // LVL = int of level (lvl 9, lvl 12, etc)
 // DFB = int of amount of defeated bosses
 function generateLevel(LVL, DFB) {
-    for (let i = 0; i < 99; i++) { for (let j = 0; j < 99; j++) { levelGrid[i,j] = empty }} // Resets the level to be empty.
-    levelGrid[50,50] = initial; // Sets the center of the level grid to the initial spawning room.
+    let rooms = RoomTypes()
+    let levelGrid = []
+    for(let i =0; i<99; i++){
+        levelGrid.push([])
+        for(let j = 0; j<99; j++){
+            levelGrid[i].push(null)
+        }// 99 long
+    }
+    let rnd = ["standard", "standard", "standard", "standard", "standard", "standard", "standard", "standard", "loot", "shop"];
+    for (let i = 0; i < 99; i++) { 
+        for (let j = 0; j < 99; j++) { 
+            levelGrid[i][j] = null 
+        }
+    } // Resets the level to be empty.
+    levelGrid[50][50] = rooms.initial(); // Sets the center of the level grid to the initial spawning room.
     // Creates the remaining rooms of the level.
     for (let i = 0; i < (2*DFB) + 2; i++) {
-        let room = levelGrid[random(levelGrid[])];
+        let room = levelGrid[random(levelGrid)];
         if (room )
         // room = random(rnd)
 
--- roomTypes.js
+++ roomTypes.js
@@ -2,6 +2,15 @@ class RoomTypes {
     constructor() {
         this
     }
+    initial() {
+        return 2;
+    }
+    standard() { 
+        return 3;
+    }
+    loot() {
+        return 4;
+    }
 }
 
 // Room Types:
