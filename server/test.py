f176085b33ffdbed24963ea70a41c0dd1b266137
TJ Nickerson <TJPNick@gmail.com>
2022-11-01 10:19:50 -0400

    setup python server

---
+++ server/test.py
@@ -0,0 +1,8 @@
+#! C:\Users\TJ\AppData\Local\Microsoft\WindowsApps\python.exe
+print("Content-type: text\n\n");
+print('you got it')
+from datetime import datetime
+now = datetime.now()
+file = open('test.txt','w')
+file.write("ran on " + now.strftime("%Y-%m-%d %H:%M:%S"))
+file.close()
