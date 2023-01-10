e909262780ab4eafd7afa8038310ba321c5d9aa0
Toby <thansen2024@jpkeefehs.org>
2023-01-10 08:56:55 -0500

    added book sprite

--- Assets.js
+++ Assets.js
@@ -26,6 +26,7 @@ class Assets {
         this.images.enemies = {};
         this.images.target;
         this.images.cone;
+        this.images.book;
         this.images.player = {};
         this.images.player.idle;
         this.images.player.run = [];
@@ -88,6 +89,7 @@ class Assets {
         this.images.target = loadImage('assets/sprites/target.png', loaded);
         this.images.enemies.frog = loadImage('assets/sprites/enemies/frog.png', loaded);
         this.images.cone = loadImage('assets/sprites/cone.png', loaded);
+        this.images.book = loadImage('assets/sprites/book.png', loaded);
         this.images.player.idle = loadImage('assets/sprites/idle.png', loaded);
         for (let i = 0; i < 2; i++) {
             this.images.backgrounds.push(loadImage('assets/sprites/background' + i + '.png', loaded));
